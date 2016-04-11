57b5065273de0a2a91d226384f180b52cb243d9f
Marek Langiewicz on MUbuntuY580 <marek.langiewicz@gmail.com>
2016-04-11 04:35:05 +0200

    playing with build files...

--- mycorelib/src/main/java/pl/mareklangiewicz/mycorelib/MyClass.kt
+++ mycorelib/src/main/java/pl/mareklangiewicz/mycorelib/MyClass.kt
@@ -1,7 +1,10 @@
 package pl.mareklangiewicz.mycorelib
 
+// TODO NOW: move some real code to this module - from myutils
+// TODO LATER: remove this class - it is only temporary to test gradle build scripts, jitpack, etc..
 object MyClass {
     fun bla() {
-        println("blaaa")
+        println("blaaa a a")
     }
 }
+
